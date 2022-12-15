1c199083662e90e58bcdc0e6fcb8d135e3b39648
dexterchan <dexterchan@gmail.com>
2022-12-15 22:53:21 +0800

    add assert check for pub sub

--- tests/pubsub/test_pubsub.py
+++ tests/pubsub/test_pubsub.py
@@ -49,8 +49,12 @@ async def test_publish_subscribe(event_loop):
 
 @pytest.mark.asyncio
 async def test_publish_subscribe_loop(event_loop):
-    p = nats_lab.Async_Publisher_Nats(server=test_server, port=test_port)
+    
+    returned_message = []
+    total_messages = 10
+    
 
+    p = nats_lab.Async_Publisher_Nats(server=test_server, port=test_port)
     await p.connect()
     finish_run = asyncio.Future()
 
@@ -59,19 +63,20 @@ async def test_publish_subscribe_loop(event_loop):
         id = data["id"]
         total = data["total"]
         print(data)
+        returned_message.append(data)
         if id==total:
             finish_run.set_result(f"finish {total}")
         else:
             print(f"publish new message:{id+1}")
             await p.publish(
-                subject=test_subject,
-                payloads=[{
-                    "id":id+1,
-                    "start":data["start"]+1,
-                    "end": data["end"]+1,
-                    "total":total
-                }]
-            )
+                    subject=test_subject,
+                    payloads=[{
+                        "id":id+1,
+                        "start":data["start"]+1,
+                        "end": data["end"]+1,
+                        "total":total
+                    }])
+            
         
     await p.subscribe(
         subject=test_subject, message_handler=_handler
@@ -85,3 +90,4 @@ async def test_publish_subscribe_loop(event_loop):
 
     await p.close()
     print(msg)
+    assert len(returned_message) == total_messages
