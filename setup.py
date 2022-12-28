bc92767923758cd909288592fb650c195bbc2298
dexterchan <dexterchan@gmail.com>
2022-12-28 19:49:05 +0800

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements_file_lst = ["requirements.txt", "requirements_dev.txt"]
+requirements_file_lst = ["requirements.txt"]
 install_req = []
 directory = os.getcwd()
 
@@ -25,16 +25,16 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Dexter Chan",
     author_email='dexterchan@gmail.com',
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     description="Testing nats functionality",
     entry_points={
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dexterchan/nats_lab',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
