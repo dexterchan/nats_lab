15d761cbdc30c1361ca5976d61b9950c5c41981a
dexterchan <dexterchan@gmail.com>
2022-12-31 17:41:14 +0800

    Bump version: 0.1.3 → 0.1.4

--- src/pubsub/__init__.py
+++ src/pubsub/__init__.py
@@ -2,5 +2,5 @@
 
 __author__ = """Dexter Chan"""
 __email__ = 'dexterchan@gmail.com'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 version_tuple = (0, 0, 1)
--- src/utility/__init__.py
+++ src/utility/__init__.py
@@ -1,2 +1,2 @@
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 version_tuple = (0, 0, 1)
