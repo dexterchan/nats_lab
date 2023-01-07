6e945a36812ce16ed56e43b6b4744abbb71407c3
dexterchan <dexterchan@gmail.com>
2023-01-07 18:06:39 +0800

    Bump version: 0.1.5 → 0.1.6

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dexterchan/nats_lab',
-    version='0.1.5',
+    version='0.1.6',
     zip_safe=False,
 )
